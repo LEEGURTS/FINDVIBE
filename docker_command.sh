13ebc04aa3fb67524d858e397f193f744bf7e3af
HungKungE <Jonsun59@gmail.com>
2023-05-21 15:04:27 +0900

    [docker] 파이썬 서버 추가 관련 도커 파일들 수정

--- docker_command.sh
+++ docker_command.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
 #local에서
-if [ $1 == "build" ]; then
-  sudo docker build -t hungkunge/findvibe_docker -f docker/Dockerfile .
+if [ $1 == "build_fb" ]; then
+  sudo docker build -t hungkunge/findvibe_docker -f docker/findvibe/Dockerfile .
+if [ $1 == "build_md" ]; then
+  sudo docker build -t hungkunge/findvibe_model -f docker/model/Dockerfile .
 elif [ $1 == "reset" ]; then
   sudo docker rm -f $(docker ps -qa);
   sudo docker image rm -f $(docker image ls -q);
 elif [ $1 == "push" ]; then
-  docker push hungkunge/findvibe_docker
+  docker push hungkunge/findvibe_docker;
+  docker push hungkunge/findvibe_model;
 
 #ec2에서
 elif [ $1 == "pull" ]; then
-  sudo docker pull hungkunge/findvibe_docker
+  sudo docker pull hungkunge/findvibe_docker;
+  sudo docker pull hungkunge/findvibe_model;
 elif [ $1 == "run" ]; then
-  sudo docker run -d -p 5000:5000 --network=host hungkunge/findvibe_docker
+  sudo docker run -d -p 5000:5000 --network=host hungkunge/findvibe_docker;
+  sudo docker run -d -p 5002:5002 --network=host hungkunge/findvibe_model;
 elif [ $1 == "stop" ]; then
-  sudo docker stop $(sudo docker ps -aq)
+  sudo docker stop $(sudo docker ps -aq);
 else
   echo "wrong command!"
 fi
--- predict/main.py
+++ predict/main.py
@@ -10,9 +10,16 @@ def predict_location():
   # pic_src 로 사진파일 읽기 (upload_images에 있음)
   # 사진 파일을 model에 넣기
   # 결과를 json으로 반환하기
-
+  # 반환 : [
+  #       [num_features(무시), 위도:float, 경도:float, 각도:float],
+  #       []...
+  #      ] -> 이미지 1개 (결과 n개 -> 그 중에 제일 나은 결과값 3개 리턴 -> [결과1,결과2,결과3])
+  #  
+  # 예상 : [{},{},{}]
   print(pic_src_array)
 
+
+  
   return jsonify({ 'result' : 'success' })
 
 if __name__ == "__main__":
