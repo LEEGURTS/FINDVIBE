23f6768960ccac390826564b9e9994d74ece338b
HungKungE <Jonsun59@gmail.com>
2023-06-05 02:44:55 +0900

    [predict] 딥러닝 모델 가져오기

--- docker_command.sh
+++ docker_command.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
-
 #local에서
-if [ $1 == "build_fb" ]; then
-  sudo docker build -t hungkunge/findvibe_docker -f docker/findvibe/Dockerfile .
-if [ $1 == "build_md" ]; then
-  sudo docker build -t hungkunge/findvibe_model -f docker/model/Dockerfile .
+if [ $1 == "build" ]; then
+  sudo docker build -t hungkunge/findvibe_nodejs -f docker/findvibe/Dockerfile .
 elif [ $1 == "reset" ]; then
   sudo docker rm -f $(docker ps -qa);
   sudo docker image rm -f $(docker image ls -q);
 elif [ $1 == "push" ]; then
-  docker push hungkunge/findvibe_docker;
-  docker push hungkunge/findvibe_model;
+  docker push hungkunge/findvibe_nodejs;
 
 #ec2에서
 elif [ $1 == "pull" ]; then
-  sudo docker pull hungkunge/findvibe_docker;
-  sudo docker pull hungkunge/findvibe_model;
+  sudo docker pull hungkunge/findvibe_nodejs;
 elif [ $1 == "run" ]; then
-  sudo docker run -d -p 5000:5000 --network=host hungkunge/findvibe_docker;
-  sudo docker run -d -p 5002:5002 --network=host hungkunge/findvibe_model;
+  sudo docker run -d -p 5000:5000 --network=host hungkunge/findvibe_nodejs;
 elif [ $1 == "stop" ]; then
   sudo docker stop $(sudo docker ps -aq);
 else
-  echo "wrong command!"
+  echo "wrong command!";
 fi
